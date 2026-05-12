6a0f446d621f6d8176092e97398fa89b47d4aa62
Link4Electronics <link4electronics@gmail.com>
2026-05-12 15:31:10 -0300

    Revert "Update make-appimage.sh"
    This reverts commit 5d44b77591fa4cf6968f41149e8794c23f103e47.

--- scripts/make-appimage.sh
+++ scripts/make-appimage.sh
@@ -7,81 +7,22 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	cmake             \
-	doxygen           \
-	fluidsynth        \
-	ibus			  \
-	libdecor          \
-    lua-filesystem    \
-    lua-lpeg          \
-	ninja			  \
-	pipewire-alsa     \
-	pipewire-audio    \
-	pipewire-jack     \
-	rtmidi            \
-	sdl2_mixer        \
-	timidity++		  \
-	vulkan-headers	  \
-	wayland-protocols
+	cmake           \
+	doxygen         \
+	fluidsynth      \
+	libdecor        \
+    lua-filesystem  \
+    lua-lpeg        \
+	pipewire-alsa   \
+	pipewire-audio  \
+	pipewire-jack   \
+	rtmidi          \
+	sdl2_mixer      \
+	timidity++
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
-
-sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg # because the docker image is ran as root this is needed lol
-mkdir sdl2 && cd sdl2
-cat <<-'EOF' > ./PKGBUILD
-# Maintainer: HurricanePootis <hurricanepootis@protonmail.com>
-# Contributor: Sven-Hendrik Haase <svenstaro@archlinux.org>
-pkgname=sdl2
-pkgver=2.32.8
-pkgrel=1
-pkgdesc="A library for portable low-level access to a video framebuffer, audio output, mouse, and keyboard (Version 2)"
-arch=('x86_64' 'aarch64')
-url="https://www.libsdl.org"
-license=('Zlib')
-provides=('sdl2-compat')
-conflicts=('sdl2-compat')
-depends=('glibc' 'libxext' 'libxrender' 'libx11' 'libgl' 'libxcursor' 'hidapi' 'libusb')
-makedepends=('alsa-lib' 'dbus' 'mesa' 'libpulse' 'libxrandr' 'libxinerama' 'wayland' 'libxkbcommon'
-            'wayland-protocols' 'ibus' 'libxss' 'cmake' 'jack' 'ninja' 'pipewire'
-            'libdecor' 'vulkan-driver' 'vulkan-headers' 'libsamplerate')
-optdepends=('alsa-lib: ALSA audio driver'
-            'libpulse: PulseAudio audio driver'
-            'jack: JACK audio driver'
-            'pipewire: PipeWire audio driver'
-            'libdecor: Wayland client decorations')
-source=("https://github.com/libsdl-org/SDL/releases/download/release-${pkgver}/SDL2-${pkgver}.tar.gz")
-        sha512sums=('484c33638e7bd1002815bb1f6a47a292d1eaf0b963598dde65f4a3e077dfe75ee35b9ea4b3b767365b3ef4f613c4d69ce55b5e96675de562994344e83a978272')
-          
-prepare(){
-        cd "$srcdir/SDL2-$pkgver"
-}
-          
-build() {
-        CFLAGS+=" -ffat-lto-objects"
-        cmake -S SDL2-${pkgver} -B build -G Ninja \
-              -D CMAKE_INSTALL_PREFIX=/usr \
-              -D SDL_STATIC=OFF \
-              -D SDL_RPATH=OFF
-        cmake --build build
-}
-          
-package() {
-        DESTDIR="${pkgdir}" cmake --install build
-          
-        # For some reason, this isn't named correctly and we have to fix it to reflect the actual staticlib name.
-        sed -i "s/libSDL2\.a/libSDL2main.a/g" "$pkgdir"/usr/lib/cmake/SDL2/SDL2Targets-noconfig.cmake
-          
-        install -Dm644 SDL2-${pkgver}/LICENSE.txt "$pkgdir/usr/share/licenses/$pkgname/LICENSE"
-}
-EOF
-
-makepkg -f
-pacman --noconfirm -Rsndd sdl2-compat
-pacman --noconfirm -U *.pkg.tar.*
-cd ..
-
 cmake -B build \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DENABLE_UNIT_TESTS=OFF \
